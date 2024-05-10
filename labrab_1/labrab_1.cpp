d9dc8f7e8628c0379929c33eb9d010580c66568e
Фёдор Коренюгин <f.a.koreniugin@stud.mospolytech.ru>
2024-05-11 02:47:23 +0300

    lab2_1

--- labrab_1/labrab_1.cpp
+++ labrab_1/labrab_1.cpp
@@ -1,35 +1,23 @@
 ﻿#include <iostream>
-#include <time.h>
-#define arrlength 10
-#define arrfrom 2
-#define arrto 103
+
+//рекурсивная функция
+int foo(int n, int end) {
+	
+	std::cout << n << "\n";
+	n++;
+	if (n <= end) {
+		foo(n, end);
+	}
+	return 0;
+}
+
+int main() {
+	int end{};
+	std::cout << "Enter the number n = ";
+	std::cin >> end;
+	int n = 1;
+	
+	foo(n, end);
 
 
-int main()
-{
-	int temp = NULL;
-	//создаем массив из случайных чисел
-	srand(time(NULL));
-	int Random_Array[arrlength]{};
-	for (int i{ 0 }; i <= arrlength; i++) {
-		Random_Array[i] = arrfrom + rand() % (arrto + 1);
-		std::cout << Random_Array[i] << "\t";
-	}
-
-	//алгоритм сортировки
-	for (int i{ 0 }; i <= arrlength - 1; i++) {
-		int min_ind = i;
-		for (int j{ i }; j <= arrlength; j++) {
-			if (Random_Array[j] < Random_Array[min_ind]) {
-				temp = Random_Array[j];
-				Random_Array[j] = Random_Array[i];
-				Random_Array[i] = temp;
-			}
-		}
-	}
-	//вывод полученного массива
-	std::cout << "\n";
-	for (int i{ 0 }; i <= arrlength; i++) {
-		std::cout << Random_Array[i] << "\t";
-	}
 }
