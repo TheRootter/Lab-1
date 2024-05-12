fbcf0e086c81f3c71725e68e88c3cc12ca12e27b
Фёдор Коренюгин <f.a.koreniugin@stud.mospolytech.ru>
2024-05-12 20:38:37 +0300

    ДLab3_1

--- labrab_1/labrab_1.cpp
+++ labrab_1/labrab_1.cpp
@@ -1,35 +1,63 @@
 ﻿#include <iostream>
-#include <time.h>
-#define arrlength 10
-#define arrfrom 2
-#define arrto 103
+#define END 100
+#define START 50
+#define length 10
+
+int swap(int first, int second, int* pArray) {
+	
+	int temp = pArray[second];
+	pArray[second] = pArray[first];
+	pArray[first] = temp;
+	return 0;
+}
+//быстрая сортировка
+
+int QS(int left , int right, int* pArray) {
+	int temp{ NULL };
+	int pivot;
+	int pivot_index;
+	int left_wall = left;
+	int right_wall = right;
+        pivot = pArray[left];
+        while (left < right)
+        {
+            while ((pArray[right] > pivot) && (left < right))
+                right--;
+            if (left != right) 
+            {
+                pArray[left] = pArray[right];
+                left++; 
+            }
+            while ((pArray[left] < pivot) && (left < right))
+                left++; 
+            if (left != right) 
+            {
+                pArray[right] = pArray[left]; 
+                right--; 
+            }
+        }
+        pArray[left] = pivot; 
+        pivot_index = left;
+        left = left_wall;
+        right = right_wall;
+        if (left < pivot_index) // Рекурсия
+            QS(left, pivot_index - 1, pArray);
+        if (right > pivot_index)
+            QS(pivot_index + 1, right, pArray);
+        return 0;
+    }
 
 
-int main()
-{
-	int temp = NULL;
-	//создаем массив из случайных чисел
-	srand(time(NULL));
-	int Random_Array[arrlength]{};
-	for (int i{ 0 }; i <= arrlength; i++) {
-		Random_Array[i] = arrfrom + rand() % (arrto + 1);
-		std::cout << Random_Array[i] << "\t";
+int main() {
+	int Array[length]{};
+	for (int i{ 0 }; i < length; i++) {
+		Array[i] = rand() % (END - START +1)+ START;
+		std::cout << Array[i] << "\t";
 	}
-
-	//алгоритм сортировки
-	for (int i{ 0 }; i <= arrlength - 1; i++) {
-		int min_ind = i;
-		for (int j{ i }; j <= arrlength; j++) {
-			if (Random_Array[j] < Random_Array[min_ind]) {
-				temp = Random_Array[j];
-				Random_Array[j] = Random_Array[i];
-				Random_Array[i] = temp;
-			}
-		}
+	QS(0, length-1, Array);
+	std::cout << "\n\n\n\n";
+	for (int i{ 0 }; i < length; i++) {
+		std::cout << Array[i] << "\t";
 	}
-	//вывод полученного массива
-	std::cout << "\n";
-	for (int i{ 0 }; i <= arrlength; i++) {
-		std::cout << Random_Array[i] << "\t";
-	}
+	
 }
